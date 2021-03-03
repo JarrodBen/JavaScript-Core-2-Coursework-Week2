1f41ca3675b85c71aa9681049f782e4cf72beeab
JarrodBen <46967203+JarrodBen@users.noreply.github.com>
2021-03-03 12:28:43 +0200

    C - Completed, added required images for the exercise

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,5 +1,35 @@
-function readingList(books) {
-  // Write your code here...
+function readingList() {
+  let content = document.getElementById("content");
+
+  let headingOne = document.createElement("h1")
+  headingOne.textContent = "Reading List";
+  content.appendChild(headingOne)
+
+  let bookList = document.createElement("ul")
+
+  for (let book of books) {
+    let eachBook = document.createElement("li");
+    eachBook.classList.add("readingList");
+
+    if (book.alreadyRead) {
+      eachBook.classList.add("haveRead")
+    } else {
+      eachBook.classList.add("haveNotRead")
+    }
+
+    let bookContent = document.createElement("p");
+    bookContent.textContent = `${book.title} - ${book.author}`
+
+    let bookImg = document.createElement("img");
+    bookImg.setAttribute("src", "./Images/" + `${book.title}` + ".jpg")
+    bookImg.setAttribute("alt", "This is the book cover of " + `${book.title}`)
+    bookImg.classList.add("bookCover")
+    bookImg.classList.add("bookCover")
+
+    eachBook.append(bookContent, bookImg);
+    bookList.appendChild(eachBook);
+  }
+  content.appendChild(bookList)
 }
 
 const books = [
