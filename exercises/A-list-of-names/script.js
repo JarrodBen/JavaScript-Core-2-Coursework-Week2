8b98a0b3f08c1801d5c05ed50c502ac02142c9a6
JarrodBen <46967203+JarrodBen@users.noreply.github.com>
2021-03-03 11:13:54 +0200

    A Completed

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,14 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+
+
+  for (let x = 0; x < people.length; x++) {
+    let personName = document.createElement('h1');
+    let personJob = document.createElement('h2');
+    personName.innerHTML = people[x].name
+    personJob.innerHTML = people[x].job
+    content.append(personName, personJob)
+  }
 }
 
 let people = [
