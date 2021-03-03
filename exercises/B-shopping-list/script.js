e4751fec081bdca2857a5a9bb5e580c2ed56935d
JarrodBen <46967203+JarrodBen@users.noreply.github.com>
2021-03-03 11:34:25 +0200

    B  Completed

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,13 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+function shoppingList(arrayOfItems) {
+  let content = document.getElementById("content");
+  let listUl = document.createElement('ul');
+  content.append(listUl)
+
+  for (let x = 0; x < shopping.length; x++) {
+    let itemLi = document.createElement('li');
+    itemLi.textContent = shopping[x];
+    listUl.append(itemLi)
+  }
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
