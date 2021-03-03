e3bb2164793ff955bd7c65c111fefd2840831a54
JarrodBen <46967203+JarrodBen@users.noreply.github.com>
2021-03-03 11:16:29 +0200

    A - Edited

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -5,8 +5,8 @@ function listOfNames(arrayOfPeople) {
   for (let x = 0; x < people.length; x++) {
     let personName = document.createElement('h1');
     let personJob = document.createElement('h2');
-    personName.innerHTML = people[x].name
-    personJob.innerHTML = people[x].job
+    personName.textContent = people[x].name
+    personJob.textContent = people[x].job
     content.append(personName, personJob)
   }
 }
