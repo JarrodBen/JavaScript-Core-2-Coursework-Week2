d640de24918ed8842b3b7dd8fe6663d781300052
JarrodBen <46967203+JarrodBen@users.noreply.github.com>
2021-03-03 13:27:34 +0200

    D - Completed, added a bit of css for strike-through

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,6 +1,22 @@
 function todoList(todos) {
-  // Write your code here...
+  let listUl = document.createElement("ul");
+  let content = document.getElementById("content");
+  content.append(listUl);
+
+  for (let y = 0; y < todos.length; y++) {
+    let doItemList = document.createElement("li");
+    doItemList.textContent = todos[y].todo;
+    doItemList.addEventListener("click", function() {
+      if (doItemList.classList.length === 0){
+        doItemList.className = "strike";
+      } else if (doItemList.className === "strike") {
+        doItemList.classList.remove("strike")
+      }
+    })
+    listUl.append(doItemList)
+  }
 }
+
 
 const todos = [
   {todo: "wash the dishes"}, 
