ab40f9e9973f2fa5818e18711462a07a85c1f32e
JarrodBen <46967203+JarrodBen@users.noreply.github.com>
2021-03-04 11:41:18 +0200

    Completed Exercise E

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,7 +1,34 @@
 function listOfColours(colours) {
-  // Write your code here...
+  let colourDiv = document.getElementById("content");
+  let colourP = document.createElement("p");
+
+  let colourSelect = document.createElement("select");
+  colourSelect.name = "colours";
+
+  colourDiv.append(colourSelect);
+  colourDiv.append(colourP);
+  let noneSelected = document.createElement("option");
+  colourSelect.append(noneSelected);
+
+  for (let x = 1; x < colours.length+1; x++) {
+    let colourSelected = document.createElement("option");
+    colourSelected.value = colours[x - 1];
+    colourSelected.textContent = colours[x - 1];
+    colourSelect.append(colourSelected);
 }
 
-const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
+    var selectColour = function () {
+        colourP.innerText = "You have selected: " + colourSelect.value;
+        colourP.style.color = colourSelect.value;    
+    }
+
+colourSelect.addEventListener("change", selectColour);
+}
+const colours = ["red", 
+                "blue", 
+                "green", 
+                "yellow", 
+                "pink", 
+                "brown"];
 
 listOfColours(colours);
