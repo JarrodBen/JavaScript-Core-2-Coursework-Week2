813da6743c074d7d4bf044142a076dd52054fe43
JarrodBen <46967203+JarrodBen@users.noreply.github.com>
2021-03-05 16:38:37 +0200

    Completed Bike project, slight changes to beginning code, but works properly

--- projects/1-bike-project/js/main.js
+++ projects/1-bike-project/js/main.js
@@ -1,5 +1,5 @@
-let jumboBtn = document.getElementsByClassName("jumbotron")
-let bikeBtn = document.getElementsByClassName("btn-primary")
+let jumboTrn = document.getElementsByClassName("jumbotron")
+let bikeBtn = document.getElementsByClassName("btn-lrg")
 let volBtn = document.getElementsByClassName("btn-secondary")
 
 let btnBlue = document.getElementById("blueBtn")
@@ -7,10 +7,10 @@ let btnOrange = document.getElementById("orangeBtn")
 let btnGreen = document.getElementById("greenBtn")
 
 // Functions for all three themes
-
 // Function for the blue theme
+
 var blueButn = function () {
-    jumboBtn[0].style.backgroundColor = "#588fbd"
+    jumboTrn[0].style.backgroundColor = "#588fbd"
     bikeBtn[0].style.backgroundColor = "#ffa500"
     volBtn[0].style.backgroundColor = "black"
     volBtn[0].style.color = "white"
@@ -19,8 +19,9 @@ var blueButn = function () {
 btnBlue.addEventListener("click", blueButn)
 
 //Function for the orange theme
+
 var orangeButn = function () {
-    jumboBtn[0].style.backgroundColor = "#f0ad4e"
+    jumboTrn[0].style.backgroundColor = "#f0ad4e"
     bikeBtn[0].style.backgroundColor = "#5751fd"
     volBtn[0].style.backgroundColor = "#31b0d5"
     volBtn[0].style.color = "white"
@@ -35,7 +36,7 @@ var greenButn = function () {
     volBtn[0].style.backgroundColor = "#8c9c08"
 }
 
-btnGreen.addEventListener("click", greenButn)
+btnGreen.addEventListener("click", greenButn) 
 
 let regForm = document.getElementsByTagName("form")[0]
 let emailField = document.getElementById("exampleInputEmail1")
